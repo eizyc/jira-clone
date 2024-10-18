3dc54c1984b2ef1b1162463e4c5b4aed344ba430
eizyc <yizhichen98@gmail.com>
2024-10-17 21:56:52 -0500

    feat(auth): create auth screens

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { SignInCard } from "@/features/auth/components/sign-in-card";
+
+const SignInPage = async () => {
+
+  return <SignInCard />
+};
+ 
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+const SignUpPage = async () => {
+
+  return <SignUpCard />
+};
+ 
+export default SignUpPage;
