fbde603f9c335e58801e990192f1b62ff4aeca02
eizyc <yizhichen98@gmail.com>
2024-10-19 16:06:18 -0500

    feat(workspace): add workspace form

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,14 +1,13 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 
 export default async function Home() {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
   
   return (
-    <div>
-      This is Home page
-    </div>
+    <CreateWorkspaceForm/>
   );
 };
