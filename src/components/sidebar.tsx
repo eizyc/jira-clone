2c41b7d662f3d8b2a3c7142c81d78cbcf6587203
eizyc <yizhichen98@gmail.com>
2024-10-19 23:19:49 -0500

    feat(workspace): add workspace switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { Navigation } from "./navigation";
 import { DottedSeparator } from "./dotted-separator";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -11,6 +12,7 @@ export const Sidebar = () => {
         <Image src="/logo.svg" alt="logo" width={164} height={48} />
       </Link>
       <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
       <DottedSeparator className="my-4" />
