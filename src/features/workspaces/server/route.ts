110479fdf4c0354a95458bb7bf4aa66538dae952
eizyc <yizhichen98@gmail.com>
2024-10-20 22:11:00 -0500

    feat(workspace): build the invite system

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
@@ -65,6 +66,28 @@ const app = new Hono()
     );
 
     return c.json({ data: workspace });
+  }
+)
+.get(
+  "/:workspaceId/info",
+  sessionMiddleware,
+  async (c) => {
+    const databases = c.get("databases");
+    const { workspaceId } = c.req.param();
+
+    const workspace = await databases.getDocument<Workspace>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId,
+    );
+
+    return c.json({ 
+      data: { 
+        $id: workspace.$id, 
+        name: workspace.name, 
+        imageUrl: workspace.imageUrl
+      } 
+    });
   }
 )
 .post(
@@ -218,6 +241,51 @@ const app = new Hono()
       workspaceId,
       {
         inviteCode: generateInviteCode(6),
+      },
+    );
+
+    return c.json({ data: workspace });
+  }
+)
+.post(
+  "/:workspaceId/join",
+  sessionMiddleware,
+  zValidator("json", z.object({ code: z.string() })),
+  async (c) => {
+    const { workspaceId } = c.req.param();
+    const { code } = c.req.valid("json");
+
+    const databases = c.get("databases");
+    const user = c.get("user");
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id,
+    });
+
+    if (member) {
+      return c.json({ error: "Already a member" }, 400);
+    }
+
+    const workspace = await databases.getDocument<Workspace>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId
+    );
+
+    if (workspace.inviteCode !== code) {
+      return c.json({ error: "Invalid invite code" }, 400);
+    }
+
+    await databases.createDocument(
+      DATABASE_ID,
+      MEMBERS_ID,
+      ID.unique(),
+      {
+        workspaceId,
+        userId: user.$id,
+        role: MemberRole.MEMBER,
       },
     );
 
